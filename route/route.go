7cd82bc8052b074243deddb5cfd3d62ae2d54ce1
PuerNya <safarier@outlook.com>
2024-05-26 01:09:00 +0800

    use origin fqdn when destination is fqdn
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/route.go
+++ route/route.go
@@ -321,7 +321,7 @@ func (r *Router) matchRule(
 		metadata.FakeIP = true
 		r.logger.DebugContext(ctx, "found fakeip domain: ", domain)
 	}
-	if r.dnsReverseMapping != nil && metadata.Domain == "" {
+	if r.dnsReverseMapping != nil {
 		domain, loaded := r.dnsReverseMapping.Query(metadata.Destination.Addr)
 		if loaded {
 			metadata.Domain = domain
@@ -521,7 +521,7 @@ func (r *Router) actionSniff(
 		)
 		if err == nil {
 			//goland:noinspection GoDeprecation
-			if action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
+			if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 				metadata.Destination = M.Socksaddr{
 					Fqdn: metadata.SniffHost,
 					Port: metadata.Destination.Port,
@@ -614,7 +614,7 @@ func (r *Router) actionSniff(
 				}
 				if metadata.Protocol != "" {
 					//goland:noinspection GoDeprecation
-					if action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
+					if !metadata.Destination.IsFqdn() && action.OverrideDestination && M.IsDomainName(metadata.SniffHost) {
 						metadata.Destination = M.Socksaddr{
 							Fqdn: metadata.SniffHost,
 							Port: metadata.Destination.Port,
